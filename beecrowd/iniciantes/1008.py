f45fcec354940037aace872cccddc78791609db2
NkAndre <andre270708@gmail.com>
2026-07-17 19:46:13 -0300

    exc 1008 feito

---
+++ beecrowd/iniciantes/1008.py
@@ -0,0 +1,8 @@
+numero = int(input())
+horas_trabalhadas = int (input())
+valor_hora = float (input ())
+
+calculo = horas_trabalhadas * valor_hora
+
+print(f'NUMBER = {numero}')
+print(f'SALARY = U$ {calculo:.2f}')
